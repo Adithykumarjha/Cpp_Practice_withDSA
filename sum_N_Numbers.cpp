9d0bb3f3f714892762ca9f528f24f28e5111aa8b
Adithykumarjha <147861071+Adithykumarjha@users.noreply.github.com>
2025-09-09 22:14:07 +0530

    Add files via upload

---
+++ sum_N_Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int sum=0;
+	int i;
+	cout<<"Enter the number"<<endl;
+	for(i=1;i<=10;i++){
+		cout<<i<<""<<endl;
+		sum=sum+i;
+	}
+	cout<<"The sum is:";
+	cout<<sum<<endl;
+
+}
